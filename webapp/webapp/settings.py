d0dc858f94f4126b6130772edc26d017791b5882
Julia Góra <reneechan46@gmail.com>
2026-03-27 00:26:51 +0100

    Konfiguracja bazy Neon na osobnej galezi

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -79,11 +79,13 @@ WSGI_APPLICATION = 'webapp.wsgi.application' #created as an implementation-neutr
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://neondb_owner:npg_mIdNqGb5EO2z@ep-morning-hall-abqlhg3t-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+        conn_max_age=600
+    )
 }
 
 
