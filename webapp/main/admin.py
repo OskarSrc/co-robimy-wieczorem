9820d8cd2695fd5d9044ee59aa7c3e6fe1753bbe
XenixLupus <xenixlupus@gmail.com>
2026-04-14 08:12:30 +0200

    Zmiana css w pokojach. Rozwinełam też kluby w społeczności

--- webapp/main/admin.py
+++ webapp/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Club, Profile, FriendRequest, VotingRoom, VotingRoomItem, VotingVote
 
-# Register your models here.
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'icon')
+    filter_horizontal = ('posts',)
+
+admin.site.register(Club, ClubAdmin)
