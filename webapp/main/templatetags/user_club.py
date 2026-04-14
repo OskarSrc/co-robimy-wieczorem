7682f82aaf9573ea6604bb43bdfc73dd6daade9c
XenixLupus <xenixlupus@gmail.com>
2026-04-14 08:12:30 +0200

    Zmiana css w pokojach. Rozwinełam też kluby w społeczności

---
+++ webapp/main/templatetags/user_club.py
@@ -0,0 +1,16 @@
+from django import template
+from main.models import Profile
+
+register = template.Library()
+
+@register.filter
+def user_with_club(user):
+    """Wyświetl nicka usera z kolorem i emotką klubu jeśli użytkownik do niego należy."""
+    try:
+        profile = Profile.objects.select_related('club').get(user=user)
+        if profile.club:
+            return f'<span style="color: {profile.club.color};">{user.username} {profile.club.icon}</span>'
+        else:
+            return user.username
+    except Profile.DoesNotExist:
+        return user.username
